438a429b82cecce6e8ec7a7207141e25fa16cd8d
Mr. El <elanebritton96@gmail.com>
2017-06-19 20:33:16 -0400

    Started Github Repository

---
+++ Team-6_EscapeRoom/src/escapeRoom/Game.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package escapeRoom;
+
+/**
+ * @author Mr_El
+ * Commit only the classes to the github!!!
+ */
+public class Game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
